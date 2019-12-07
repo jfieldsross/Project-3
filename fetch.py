422d7f8939ff2ed57e6853829bf3193739cecb1b
jfieldsross <j.fieldsross@gmail.com>
2019-12-06 18:37:19 -0600

    Add files via upload

--- fetch.py
+++ fetch.py
@@ -26,51 +26,72 @@ class Fetch:
 
         # if the second element in the buffer is -1, there is empty space
         # if there is no empty space, the fetch unit stalls
-        emptyRoomInPreIssueBuffer = self.preIssueBuff[1] == -1
+        emptyRoomInPreIssueBuffer = self.preIssueBuff[3] == -1
 
         # check to see if cache contains next instruction
         # instructionIndex = the index of the next instruction to be fetched
         instructionIndex = (self.PC - 96) / 4
         # cacheHit = boolean that equals true if there is a cache hit
-        # instruction = the instruction that was stored in the cache
+
+        numIssued = 0
         cacheHit = self.cache.checkCache(-1, instructionIndex, -1, -1)
+        notBranch = True
 
-        notBranch = False
-        notBreak = True
-        hazard = False
+        while numIssued < 2 and emptyRoomInPreIssueBuffer and instructionIndex < self.numInstrs and cacheHit \
+                and notBranch:
 
-        if cacheHit:
-            #TODO decode the instruction
-            print("decode instruction here")
-            if self.opcodeStr[instructionIndex] == "B":
-                self.PC = self.PC + ((4 * self.arg1[instructionIndex]) - 4)
+            notBranch = False
+            notBreak = True
+            hazard = False
+            # before fetching following conditions must be true
+            #notStalled = notBranch and cacheHit and emptyRoomInPreIssueBuffer
 
-            elif self.opcodeStr[instructionIndex] == "CBZ":
-                if self.R[self.arg2[instructionIndex]] == 0:
-                    for i in range(len(self.preIssueBuff)):
-                        if self.preIssueBuff[i] != -1:
-                            if self.src1Reg[instructionIndex] == self.destReg[self.preIssueBuff[i]] or self.src2Reg[instructionIndex] == self.destReg[self.preIssueBuff[i]]:
-                                hazard = True
-                    if hazard != True:
-                        self.PC = self.PC + ((4 * self.arg1[instructionIndex]) - 4)
-            elif self.opcodeStr[instructionIndex] == "CBNZ":
-                if self.R[self.arg2[instructionIndex]] != 0:
-                    for i in range(len(self.preIssueBuff)):
-                        if self.preIssueBuff[i] != -1:
-                            if self.src1Reg[instructionIndex] == self.destReg[self.preIssueBuff[i]] or self.src2Reg[instructionIndex] == self.destReg[self.preIssueBuff[i]]:
-                                hazard = True
-                    if hazard != True:
-                        self.PC = self.PC + ((4 * self.arg1[instructionIndex]) - 4)
+            if cacheHit:
+                #TODO decode the instruction
 
+                if self.opcodeStr[instructionIndex] == "B": #Branch w/o checks
+                    self.PC = self.PC + ((4 * self.arg1[instructionIndex]) - 4)
 
+                elif self.opcodeStr[instructionIndex] == "CBZ": #Conditional Branch if Zero and checks for hazard
+                    if self.R[self.arg2[instructionIndex]] == 0:
+                        for i in range(len(self.preIssueBuff)):
+                            if self.preIssueBuff[i] != -1:
+                                if self.src1Reg[instructionIndex] == self.destReg[self.preIssueBuff[i]] or self.src2Reg[instructionIndex] == self.destReg[self.preIssueBuff[i]]:
+                                    hazard = True
+                        if not hazard: #if there's no hazards jump PC
+                            self.PC = self.PC + ((4 * self.arg1[instructionIndex]) - 4)
 
-            #TODO if it's not a branch instruction set to True
-            notBranch = True
+                elif self.opcodeStr[instructionIndex] == "CBNZ": #Conditional Branch if not zero and checks for hazard if hazard clear hazard
+                    if self.R[self.arg2[instructionIndex]] != 0:
+                        for i in range(len(self.preIssueBuff)):
+                            if self.preIssueBuff[i] != -1:
+                                if self.src1Reg[instructionIndex] == self.destReg[self.preIssueBuff[i]] or self.src2Reg[instructionIndex] == self.destReg[self.preIssueBuff[i]]:
+                                    hazard = True
+                        if not hazard:
+                            self.PC = self.PC + ((4 * self.arg1[instructionIndex]) - 4)
 
-            #TODO if it's a break instruction set to False
-            notBreak = False
+                elif self.opcodeStr[instructionIndex] == "NOP":
+                    self.PC += 4
+                    notBranch = True
+
+                elif self.opcodeStr[instructionIndex] == "BREAK":
+                    notBreak = False
+                    notBranch = True
+
+                else: # Let's Fetch!
+                    notBranch = True
+                    i = 0
+                    while self.preIssueBuff[i] != -1:
+                        i += 1
+                    self.preIssueBuff[i] = instructionIndex
+                    self.PC += 4
+                    instructionIndex = (self.PC - 96) / 4
+                    numIssued += 1
+
+            cacheHit = self.cache.checkCache(-1, instructionIndex, -1, -1)
+
 
         #before fetching following conditions must be true
-        notStalled = notBranch and cacheHit and emptyRoomInPreIssueBuffer
+        #notStalled = notBranch and cacheHit and emptyRoomInPreIssueBuffer
 
         return notBreak
